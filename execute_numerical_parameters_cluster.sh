2f9f238fd5f93c31f7b5b7360d3d59571730f231
Rutger Biezemans <rutgerbiezemans@hotmail.com>
2022-01-12 19:18:10 +0100

    ADD EXPERIMENT FILES The execute and parameter files here are designed for testing different methods with different parameters on the same physical problem (all in parallel) All methods are tested with and without multiscale test functions, with and without bubbles The parameters for these tests are located in subfolders of experiment_parameters The bash script governing the execution is execute_numerical_parameters_cluster.sh

---
+++ execute_numerical_parameters_cluster.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Consecutive executions of FreeFem++ code to perform experiments with different parameter values
+# The parameter values are changed by continuously changing the parameter files from experiment_parameters
+# The execution commands for FreeFem++ are adapted to use on the cluster of CERMICS
+
+# The tests executed here are supposed to change numerical parameters, but not the physical onces
+# since the reference solution is computed only once
+# Make sure the first runs use bubble functions, so all other methods can be loaded
+
+
+# FreeFem++ EXECUTION
+NUMBER_OF_TESTS = 6 #actually, the real number of tests (per type) minus 1
+NUMBER_OF_PROC = 8
+
+cp "experiment_parameters/testMS_0_wB/parameters_0.txt" parameters.txt 
+/usr/bin/mpirun -np 1 /usr/local/bin/FreeFem++-mpi main_REF.edp
+
+for NUM in $NUMS
+do 
+    cp "experiment_parameters/testMS_0_wB/parameters_$NUM.txt" parameters.txt
+    /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LIN_MPI.edp -o compute
+    /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LINOS_MPI.edp -o compute
+    /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_CR_MPI.edp -o compute
+    /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_CROS_MPI.edp -o compute
+done
+
+for TEST_TYPE in "testMS_1_wB testMS_0_nB testMS_0_wB"
+    for NUM in $NUMS
+    do 
+        cp "experiment_parameters/$TEST_TYPE/parameters_$NUM.txt" parameters.txt
+        /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LIN_MPI.edp -o load
+        /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LINOS_MPI.edp -o load
+        /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_CR_MPI.edp -o load
+        /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_CROS_MPI.edp -o load
+    done
+done
