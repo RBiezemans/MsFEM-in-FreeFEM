e42f03a9d79ba99626a6eb0e45d3d933c713b228
Rutger Biezemans <rutgerbiezemans@hotmail.com>
2022-01-12 17:55:00 +0100

    ADDITION OF LOAD ROUTINES The offline phase can be loaded, which is entirely described by the files offline_load.idp and offline_load_MPI.idp All main routines where adapted in order to allow for the choice between computing or loading the offline stage The choice is made in practice by specifing an -o argument on the command line when launching the program Some commet changes where also made in other msfem block files

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# delete files that are created during testing/developing/debugging MsFEM methods
-
-rm sol*
-rm err*
-rm parameters_*
-rm compare*.txt
-rm -r basis_* 
-rm -r effective_coeff_*
