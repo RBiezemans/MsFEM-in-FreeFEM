067d98e6628142ad251e94607f63d74ff5974c76
Rutger Biezemans <rutgerbiezemans@hotmail.com>
2022-01-12 19:50:14 +0100

    DEBUG execute_numerical_parameters_cluster.sh (for loop)

--- execute_numerical_parameters_cluster.sh
+++ execute_numerical_parameters_cluster.sh
@@ -16,7 +16,7 @@ NUMBER_OF_PROC = 8
 cp "experiment_parameters/testMS_0_wB/parameters_0.txt" parameters.txt 
 /usr/bin/mpirun -np 1 /usr/local/bin/FreeFem++-mpi main_REF.edp
 
-for NUM in $NUMS
+for NUM in $(seq 0 $NUMS)
 do 
     cp "experiment_parameters/testMS_0_wB/parameters_$NUM.txt" parameters.txt
     /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LIN_MPI.edp -o compute
@@ -26,7 +26,7 @@ do
 done
 
 for TEST_TYPE in "testMS_1_wB testMS_0_nB testMS_0_wB"
-    for NUM in $NUMS
+    for NUM in $(seq 0 $NUMS)
     do 
         cp "experiment_parameters/$TEST_TYPE/parameters_$NUM.txt" parameters.txt
         /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi main_LIN_MPI.edp -o load
