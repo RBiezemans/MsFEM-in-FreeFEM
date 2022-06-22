95897de9ff919d93b1a639c96384d06af9c65cfb
Rutger Biezemans <rutgerbiezemans@hotmail.com>
2022-06-22 16:58:19 +0200

    Prepare bash script for tests in arxiv article

--- experiment/execute_experiment_arxiv_220414.sh
+++ experiment/execute_experiment_arxiv_220414.sh
@@ -4,34 +4,40 @@
 
 # Consecutive executions of FreeFem++ code to perform experiments with different parameter values
 # The parameter values are changed continuously based on the values given below
-# Check that all values that are not controlled here, are as desired in the basic parameter.txt file in experiment_parameters
-# The execution commands for FreeFem++ are adapted to use on the cluster of CERMICS
+# The execution commands for FreeFem++-mpi may need to be changed depending on the local FreeFEM installation
 
 
 # Number of processors to be used
-NUMBER_OF_PROC=2
+NUMBER_OF_PROC=10
 
 # Parameter values to be used in the tests (all will be combined)
 # eg TOTEST_LARGE_N="8 16 32" to test for three different (coarse) mesh sizes
 TOTEST_L="1."
-TOTEST_LARGE_N="100" 
-TOTEST_LARGER_N="500" 
-TOTEST_SMALL_N="4"
-TOTEST_EPS="25"
-TOTEST_2LOGALP="-2"
-TOTEST_THETA="0.15"
-TOTEST_CONT="7"
-TOTEST_OSCOEF="1.2"
+TOTEST_LARGE_N="1024" 
+TOTEST_LARGER_N="0" # unused in this experiment 
+LARGER_N_THR="-8" # a larger value of N can be used when 2logalp is below a certain threshold
+TOTEST_SMALL_N="4 8 16 32 64 128 256"
+TOTEST_EPS="0.020947"
+TOTEST_2LOGALP="0"
+TOTEST_THETA="0"
+TOTEST_CONT="100"
+TOTEST_VFFILE="diffusion.idp"
+TOTEST_OSCOEF="0"
+TOTEST_GLUE="dof" # either "dof" or "restrict" -- without OS, this option is automatically ignored
 TOTEST_STR_DIR="0"
-TOTEST_USE_B="1 0" # it is important to treat bubbes first, so the offline stages without bubbles can be loaded
-TOTEST_TREAT_B="in_system out_system"
-TOTEST_ADV_MS="1"
-TOTEST_MS="0"
+TOTEST_USE_B="0" # it is important to treat bubbes first, so the offline stages without bubbles can be loaded
+TOTEST_TREAT_B="out_system" # either "out_system" or "in_system" -- without bubbles, this option is automatically ignored
+## TOTEST_ADV_MS="1" # has become irrelevant
+TOTEST_MS="1 0"
 
 ## SOME PARAMETER RULES
 ## - Usage of bubbles must be executed before non-usage: "1 0" or "0"
 ## - It is more efficient to use testMS before not using it
-## - It is more efficient to use in_system before using out_system
+## - It is more efficient to use bubbles in_system before using out_system
+
+
+##
+##
 
 
 # LOOP OVER ALL PARAMETER VALUES AND FreeFem++ EXECUTION
@@ -41,7 +47,7 @@ for TEST_2LOGALP in $TOTEST_2LOGALP; do sed -i "s/2logalpha=.*/2logalpha= $TEST_
 for TEST_THETA in $TOTEST_THETA; do sed -i "s/theta=.*/theta= $TEST_THETA/" "experiment/parameters.txt" 
 for TEST_CONT in $TOTEST_CONT; do sed -i "s/cont=.*/cont= $TEST_CONT/" "experiment/parameters.txt" 
 for TEST_LARGE_N in $TOTEST_LARGE_N; do sed -i "s/N=.*/N= $TEST_LARGE_N/" "experiment/parameters.txt"
-    if [ $TEST_2LOGALP = "-8" ] # the last test requires a finer reference solution (skipped here)
+    if (( $(echo "$TEST_2LOGALP <= $LARGER_N_THR" | bc) ))
     then sed -i "s/N=.*/N= $TOTEST_LARGER_N/" "experiment/parameters.txt" 
     fi
 
@@ -49,12 +55,17 @@ for TEST_LARGE_N in $TOTEST_LARGE_N; do sed -i "s/N=.*/N= $TEST_LARGE_N/" "exper
     sed -i "s/treatB=.*/treatB= out_system/" "experiment/parameters.txt"
     # The reference solution does not involve any bubble computations
     cp experiment/parameters.txt parameters.txt
+    for TEST_VFFILE in $TOTEST_VFFILE
+        do cp variational_forms/$TEST_VFFILE vffile.idp
+        break # the availibility of a vffile is only needed for compatibility with initialization, but not actually used in the computation of the reference solution
+    done
     /usr/bin/mpirun -np 1 /usr/local/bin/FreeFem++-mpi main_REF.edp -v 0
-    
+
+    for TEST_VFFILE in $TOTEST_VFFILE; do cp variational_forms/$TEST_VFFILE vffile.idp
     for TEST_SMALL_N in $TOTEST_SMALL_N; do sed -i "s/n=.*/n= $TEST_SMALL_N/" "experiment/parameters.txt" 
-    for TEST_OSCOEF in $TOTEST_OSCOEF; do sed -i "s/osCoef=.*/osCoef= $TEST_OSCOEF/" "experiment/parameters.txt" 
     for TEST_STR_DIR in $TOTEST_STR_DIR; do sed -i "s/strong_Dirichlet=.*/strong_Dirichlet= $TEST_STR_DIR/" "experiment/parameters.txt" 
-    for TEST_ADV_MS in $TOTEST_ADV_MS; do sed -i "s/advMS=.*/advMS= $TEST_ADV_MS/" "experiment/parameters.txt" 
+    for TEST_OSCOEF in $TOTEST_OSCOEF; do sed -i "s/osCoef=.*/osCoef= $TEST_OSCOEF/" "experiment/parameters.txt" 
+    for TEST_GLUE in $TOTEST_GLUE; do sed -i "s/glue=.*/glue= $TEST_GLUE/" "experiment/parameters.txt"
     # The above loops contain all parameters that require the computation of a new basis
     COMPUTE_BASIS=0
     for TEST_MS in $TOTEST_MS; do sed -i "s/testMS=.*/testMS= $TEST_MS/" "experiment/parameters.txt" 
@@ -68,27 +79,19 @@ for TEST_LARGE_N in $TOTEST_LARGE_N; do sed -i "s/N=.*/N= $TEST_LARGE_N/" "exper
             if [ $COMPUTE_BASIS == 0 ]
             then 
                 /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_LIN_MPI.edp -o compute
-                /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_CR_MPI.edp -o compute
+                # /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_CR_MPI.edp -o compute
                 COMPUTE_BASIS=1
             else
                 /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_LIN_MPI.edp -o load
-                /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_CR_MPI.edp -o load
+                # /usr/bin/mpirun -np $NUMBER_OF_PROC /usr/local/bin/FreeFem++-mpi -v 0 main_CR_MPI.edp -o load
             fi
             rm parameters.txt
         fi
 
-    done done done done done done done # end of loops over numerical parameters
-done done done done done done # end of loops over reference solution/physical parameters + fine mesh
+    done done done # end of loops over bubbles and multiscale usage
+    if (( $(echo "$TEST_OSCOEF < 0.1" | bc) )) # basic calculator is used for decimal arithmetic
+        then break # break the loop over GLUE options if no oversamling is actually used
+    fi
+    done done done done done # end of loops over basis specification
+done done done done done done # end of loops over reference solution/physical parameters + fine mesh + vf form used
 
-# Compress results into zip files
-cd results/
-zip zip_Lin_short err_Lin_MPI* solCoarse*_Lin_MPI*
-zip zip_LinOS_short err_LinOS_MPI* solCoarse*_LinOS_MPI*
-zip zip_CR_short err_CR_MPI* solCoarse*_CR_MPI*
-zip zip_CROS_short err_CROS_MPI* solCoarse*_CROS_MPI*
-
-
-# zip -r zip_Lin_all *Lin_MPI*
-# zip -r zip_LinOS_all *LinOS_MPI*
-# zip -r zip_CR_all *CR_MPI*
-# zip -r zip_CROS_all *CROS_MPI*
